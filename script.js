2901dc4ea3c99dfd38579a5a6602454356e0d51d
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-16 19:20:22 +0530

    arithmetic operator in js
    +,-,*,/,**,% are airthmatic operator
    --a, ++a are predecrement and preincrement
    a--, a++ are postdecrement and postincrement

---
+++ script.js
@@ -0,0 +1,24 @@
+// arithmetic operator in js
+
+let a = 5;
+let b = 2;
+
+console.log("a =",a,"b =",b);
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a*b =",a*b);
+console.log("a/b =",a/b);
+console.log("a%b =",a%b);
+console.log("a**b =",a**b);
+
+
+// Unary operator in airthmetic
+
+console.log("a =",a);
+console.log("a++ =",a++);
+console.log("a =",a);
+
+console.log("++a =",++a);
+console.log("a =",a);
+
+console.log("--a =",--a);
