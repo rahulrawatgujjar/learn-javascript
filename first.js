f1012d83526ebc918183ed2d915fa2f65eca38fe
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-16 17:46:40 +0530

    there are 2 types of data types in js
    1. premitive: these are 7
    2. objects

--- first.js
+++ first.js
@@ -1,5 +1,30 @@
-{
-  let isFollow = true;
-}
+let name = "Rahul";
+let age = 23;
+let marks = 88.8;
+let isPass = true;
+let a;
+let b = null;
+let c = BigInt("12345");
+let d = Symbol("helloworld!");
 
-let isFollow = true;
+
+// Here is console result:
+
+// typeof name;
+// 'string'
+// typeof marks;
+// 'number'
+// typeof isPass;
+// 'boolean'
+// typeof a;
+// 'undefined'
+// typeof b;
+// 'object'
+// c;
+// 12345n
+// typeof c;
+// 'bigint'
+// d;
+// Symbol(helloworld!)
+// typeof d;
+// 'symbol'
