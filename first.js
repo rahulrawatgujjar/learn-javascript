1d6259481a665036a276a4f371cf8dbaf4192aad
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-16 18:01:19 +0530

    objects are collection of key-value pair

--- first.js
+++ first.js
@@ -1,30 +1,15 @@
-let name = "Rahul";
-let age = 23;
-let marks = 88.8;
-let isPass = true;
-let a;
-let b = null;
-let c = BigInt("12345");
-let d = Symbol("helloworld!");
+const student = {
+  name : "rahul",
+  age : 23,
+  marks : 88.9,
+  isPass : true
+};
 
+console.log(student);
 
-// Here is console result:
+console.log(student["name"]);
+console.log(student.name);
 
-// typeof name;
-// 'string'
-// typeof marks;
-// 'number'
-// typeof isPass;
-// 'boolean'
-// typeof a;
-// 'undefined'
-// typeof b;
-// 'object'
-// c;
-// 12345n
-// typeof c;
-// 'bigint'
-// d;
-// Symbol(helloworld!)
-// typeof d;
-// 'symbol'
+console.log(student.marks);
+student.marks += 1;
+console.log(student.marks);
