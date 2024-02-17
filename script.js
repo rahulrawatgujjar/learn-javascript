16b3ba95ebf7f49d5a984b63228ec8b5b3c7d665
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-17 17:02:59 +0530

    string interpolation is like formating in python

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // string in js
 let myName= "Rahul";
 console.log(myName.length);
-
 console.log(myName[0]);
+
 
 // template literals
 let str = `this is a template literal`;
 console.log(str);
+
+
+// string interpolation
+const student={
+  name: "surender",
+  age: 23
+};
+let para = `hello i am ${student.name} and my age is ${student.age}`;
+console.log(para);
