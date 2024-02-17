0832fb23097073dc2ea0c24c0a64c7d8c43e5954
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-18 00:02:02 +0530

    array in js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let name= prompt("Enter your name:");
-name = name.replace(" ","");
-let username="@" + name + name.length;
-console.log(username);
+let marks= [97,35,25,54];
+console.log(marks);
+
+// type of array 
+console.log(typeof marks);
+
+// length of array
+console.log(marks.length);
+
+// access an element using index
+console.log(marks[0]);
+
+// modifying value at an index
+marks[0]=77;
+console.log(marks);
+
+// array may contain different types of data
+let items=["car",true,12,null,4.4];
+console.log(items);
