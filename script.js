ed26a644500491abdf4a4619a633af027ab437d7
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-17 15:25:34 +0530

    practice-set-2 question-2

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let n= prompt("Enter a number:");
-if (n%5==0){
-  console.log("Yes");
-} else{
-  console.log("No");
+let score = prompt("Enter score:");
+let grade;
+
+if (score<=100 && score>=90){
+  grade="A";
+} else if (score>=70){
+  grade="B";
+} else if (score>=60){
+  grade="C";
+} else if (score>=50){
+  grade="D";
+} else if (score>=0){
+  grade="E"
 }
+
+console.log(grade);
