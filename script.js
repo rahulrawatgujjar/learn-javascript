ff3d1ff2759d0779312ab96dfcc335a3cae4404b
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-18 00:16:22 +0530

    practice-set-4 question-1 \n Find average of marks

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-let marks= [97,35,25,54];
-console.log(marks);
-
-// type of array 
-console.log(typeof marks);
-
-// length of array
-console.log(marks.length);
-
-// access an element using index
-console.log(marks[0]);
-
-// modifying value at an index
-marks[0]=77;
-console.log(marks);
-
-// array may contain different types of data
-let items=["car",true,12,null,4.4];
-console.log(items);
-
-// print array using for loop
-let cities=["bhiwani","hisar","rohtak","delhi"];
-for(let i=0; i<cities.length; i++){
-  console.log(cities[i]);
+let marks=[97,35,25,54];
+let sum=0;
+for(let val of marks){
+  sum+=val;
 }
-
-// using for-of loop
-for(let city of cities){
-  console.log(city.toUpperCase());
-}
+console.log(`Average marks of class: ${sum/marks.length}`);
