043cae592b294296f0b7bdd4522d120970739809
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-17 16:37:30 +0530

    practice-set-3 question-2
    user will guess a number till he got right one

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-for(let i=0;i<=100;i++){
-  if (i%2==0){
-    console.log(i);
+let gameNum=33;
+let userNum;
+while (true){
+  userNum=prompt("Guess the number:");
+  if( gameNum==userNum){
+    console.log("you guess correct number ",userNum)
+    break;
   }
 }
