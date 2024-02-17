a7fd4268900cc8a6e5d705ec35163200c526bdfd
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-17 17:42:10 +0530

    practice-set-3 question-3

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-// string methods
-
-let str= "raHul";
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-
-// to remove white space
-console.log("  hello ".trim());
-
-// slicing
-console.log(str.slice(0,3));
-console.log(str.slice(3));
-
-// concatination
-let str1="rahul";
-let str2="rawat";
-let result= str1.concat(str2);
-console.log(result);
-console.log(str1+str2);
-
-// replace ( and replaceall()
-let exp= "rahulrawat";
-console.log(exp.replace("r","n"));
-console.log(exp.replaceAll("r","n"));
-
-// charAt
-console.log(str.charAt(2));
+let name= prompt("Enter your name:");
+name = name.replace(" ","");
+let username="@" + name + name.length;
+console.log(username);
