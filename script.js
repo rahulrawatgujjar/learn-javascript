8a6edf9211eb23f144cdbf943529c72b21ff97e6
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-18 19:27:12 +0530

    filter method in arrays

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ let newArray = nums.map((value)=>{
   return value**3;
 });
 
-console.log(newArray);
+console.log(newArray); // [1, 8, 27, 64]
+
+
+
+// filter method: to filter element based on some condition. Elements for which condition is true get stored in array.
+
+let items = [1,3,53,5,4,6,7];
+
+let newItems = items.filter((value)=>{
+  return value%2==0;
+});
+
+console.log(newItems);
