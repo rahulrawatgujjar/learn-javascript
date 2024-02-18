6bd2183b4c3e4b494cf7534ea3e0cb6a94557d3b
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-18 22:37:57 +0530

    pracitice-set-5 question-4

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// filter 90+ marks from marks array
-let marks= [23,34,98,89,95,68];
-console.log(marks);
+// take n as input | create an array contain 1 to n numbers | find sum and product using reduce method
+const n= prompt("Enter n:");
+let numbers=[];
+for(let i=0; i<n; i++){
+  numbers.push(i+1);
+}
+console.log("numbers:",numbers);
 
-let plusMarks= marks.filter((val)=>{
-  return val>90;
+const sum= numbers.reduce((prev,curr)=>{
+  return curr+prev;
 });
+console.log("sum:",sum);
 
-console.log(plusMarks);
+const product= numbers.reduce((prev,curr)=>{
+  return prev*curr;
+});
+console.log("product:",product);
