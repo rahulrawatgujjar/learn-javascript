b06b76b7b7079596560df633ab7382021169a832
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-18 22:25:56 +0530

    pracitice-set-5 question-3

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-// map method: it is like forEach but it can return values that can be assigned in an array
+// filter 90+ marks from marks array
+let marks= [23,34,98,89,95,68];
+console.log(marks);
 
-let nums=[1,2,3,4];
-
-let newArray = nums.map((value)=>{
-  return value**3;
+let plusMarks= marks.filter((val)=>{
+  return val>90;
 });
 
-console.log(newArray); // [1, 8, 27, 64]
-
-
-
-// filter method: to filter element based on some condition. Elements for which condition is true get stored in array.
-
-let items = [1,3,53,5,4,6,7];
-
-let newItems = items.filter((value)=>{
-  return value%2==0;
-});
-
-console.log(newItems);
-
-
-
-// reduce method: perform some operations and reduce the array to a single value. It returns that single value.
-
-let array1 = [1,2,3,4,2];
-// find sum
-const sumArray1= array1.reduce((res,curr)=>{
-  return res+curr;
-});
-
-console.log(sumArray1);
-
-// find maximum element
-const maxArray1= array1.reduce((res,curr)=>{
-  return curr>res ? curr : res;
-});
-
-console.log(maxArray1);
+console.log(plusMarks);
