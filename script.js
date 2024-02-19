ea6106d85a6aa6f1f5f976b39f3bb1a9f9ec23de
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 22:39:14 +0530

    DOM manipulation properties: tagName

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-// get html elements
+// DOM Manipulation 
+//  properties
 
-// using id
-let heading= document.getElementById("my-id");
-console.log(heading);
+// tagName
+let heading= document.querySelector("#heading");
 console.dir(heading);
-
-// using className
-let classItems= document.getElementsByClassName("my-class");
-console.dir(classItems);
-
-// using TagName
-let items= document.getElementsByTagName("p");
-console.dir(items);
-
-
-// using querySelector
-
-let idItemsQ= document.querySelector("#my-id");
-console.dir(idItemsQ);
-
-let classItemQ= document.querySelector(".my-class");
-console.dir(classItemQ);
-
-let classItemsQ= document.querySelectorAll(".my-class");
-console.dir(classItemsQ);
-
-let tagItemQ= document.querySelector("p");
-console.dir(tagItemQ);
-let tagItemsQ= document.querySelectorAll("p");
-console.dir(tagItemsQ);
+console.log(heading.tagName);
