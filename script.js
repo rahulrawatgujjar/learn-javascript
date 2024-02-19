4f2f9093d1d9d2bb328c1ffb08cf23d1396522f5
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 22:59:07 +0530

    innerHTML property

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// innerText
+// innerHTML:
+//    return plain text and html content of element and its children
+
 let div= document.querySelector("div");
 console.dir(div);
-console.log(div.innerText);
-// changing innerText
-div.innerText = "hello it changed";
+console.log(div.innerHTML);
+                            // <h1>This is heading</h1>
+                            // <div>This is a div</div>
+
+div.innerHTML="<button>click me</button>";
