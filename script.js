ebf096c67c3af48a0212f514235399b494cc776c
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 22:13:24 +0530

    get elements

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-// document object model
-console.log(window.document);
-console.dir(window.document);
+// get html elements
 
-document.body.childNodes[1].style.background="green";
-document.body.childNodes[5].innerText="It is changed using js";
+// using id
+let heading= document.getElementById("my-id");
+console.log(heading);
+console.dir(heading);
+
+// using className
+let classItems= document.getElementsByClassName("my-class");
+console.dir(classItems);
+
+// using TagName
+let items= document.getElementsByTagName("p");
+console.dir(items);
+
+
+// using querySelector
+
+let idItemsQ= document.querySelector("#my-id");
+console.dir(idItemsQ);
+
+let classItemQ= document.querySelector(".my-class");
+console.dir(classItemQ);
+
+let classItemsQ= document.querySelectorAll(".my-class");
+console.dir(classItemsQ);
+
+let tagItemQ= document.querySelector("p");
+console.dir(tagItemQ);
+let tagItemsQ= document.querySelectorAll("p");
+console.dir(tagItemsQ);
