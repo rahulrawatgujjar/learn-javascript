2246916b555ef5cfa2af69b83377be3e05dc3d96
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 23:02:58 +0530

    textContent property

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// innerHTML:
-//    return plain text and html content of element and its children
+// textContent:
+//  return textual content and also for hidden elements
 
 let div= document.querySelector("div");
-console.dir(div);
-console.log(div.innerHTML);
-                            // <h1>This is heading</h1>
-                            // <div>This is a div</div>
-
-div.innerHTML="<button>click me</button>";
+console.log(div.innerText);
+console.log(div.textContent);
