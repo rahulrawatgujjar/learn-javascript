3a200a6b861552d6cd878e2c508be2d30d92557f
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 16:16:12 +0530

    learning DOM part 1 ........ starter code

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-// take n as input | create an array contain 1 to n numbers | find sum and product using reduce method
-const n= prompt("Enter n:");
-let numbers=[];
-for(let i=0; i<n; i++){
-  numbers.push(i+1);
-}
-console.log("numbers:",numbers);
-
-const sum= numbers.reduce((prev,curr)=>{
-  return curr+prev;
-});
-console.log("sum:",sum);
-
-const product= numbers.reduce((prev,curr)=>{
-  return prev*curr;
-});
-console.log("product:",product);
+console.log("hello!");
+alert("hello!");
