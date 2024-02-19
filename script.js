2c297b573a83d161efca103166adae8a602f705f
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-19 16:55:27 +0530

    Document object model is created by browser when a webpage is loaded
    it is used to access html code using javascript and performing dynamic changes by manipulting html

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("hello!");
-alert("hello!");
+// document object model
+console.log(window.document);
+console.dir(window.document);
+
+document.body.childNodes[1].style.background="green";
+document.body.childNodes[5].innerText="It is changed using js";
