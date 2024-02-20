30926624182c281d86f942e5200319f51102c9ee
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-20 19:38:44 +0530

    practice-set-7 question-1

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// DOM manipulation: insert elements
+// create a new button element with text "click me", background color red and text color white.
+//  Then insert it into body as first element
 
-let div = document.querySelector("div");
-
-let button = document.createElement("button");
-button.innerText= "click me!";
-console.log(button);
-
-div.after(button);
-
-button.remove();
+let body= document.querySelector("body");
+console.log(body);
+let button= document.createElement("button");
+button.innerText="Click me";
+button.style.backgroundColor="red";
+button.style.color="white";
+body.prepend(button);
