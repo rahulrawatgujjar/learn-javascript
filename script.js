ddd9231cdca58d0c8df3a2a622b2235366052ae8
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-20 22:03:56 +0530

    chapter-8 ( Events in js

--- script.js
+++
@@ -1,8 +0,0 @@
-// create a paragraph in body with class "content"
-//  append a new class to this paragraph tag using js
-let para = document.querySelector(".content");
-console.log(para);
-
-para.classList.add("my-class");
-
-para.classList.remove("content");
