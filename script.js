9bd92282560e18a1be4a4b3d6461a2716a5a39c4
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-20 17:29:28 +0530

    insert at the start of node (inside)

--- script.js
+++ script.js
@@ -6,9 +6,5 @@ let button = document.createElement("button");
 button.innerText= "click me!";
 console.log(button);
 
-// append: insert at the end of element
-div.append(button);
-
-// insert at the begining of element
 div.prepend(button);
 
