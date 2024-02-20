f1fdd7f0c5fc8f69c99b0c24c67d91796c2a4037
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-20 19:53:10 +0530

    parctice-set-7 question-2

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// create a new button element with text "click me", background color red and text color white.
-//  Then insert it into body as first element
+// create a paragraph in body with class "content"
+//  append a new class to this paragraph tag using js
+let para = document.querySelector(".content");
+console.log(para);
 
-let body= document.querySelector("body");
-console.log(body);
-let button= document.createElement("button");
-button.innerText="Click me";
-button.style.backgroundColor="red";
-button.style.color="white";
-body.prepend(button);
+para.classList.add("my-class");
+
+para.classList.remove("content");
