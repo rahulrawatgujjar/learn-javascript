07c7dbdf98e15c81d8d7f2a4a07dff1f7042edf1
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-23 17:57:23 +0530

    error handling in js

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-// You are creating a website for your college.
-// create a class User with 2 properties name and email.
-// it has a method viewData that allow user to view website data
+// error handling 
 
-// add an admin class that inherit from user class and have edit access to data
+let a= 3;
+let b= 5;
 
-let data= "This is a webpage of College website";
+console.log("a:",a,"b:",b);
+console.log("a+b:",a+b);
 
-class User{
-  constructor(name,email){
-    this.name=name;
-    this.email=email;
-  }
-
-  viewData(){
-    console.log(`Data:\n${data}`);
-  }
+try{
+  console.log("a*b:",a*c); // here is the error
+} catch(err){
+  console.log(err);
 }
 
-class Admin extends User{
-  constructor(name,email){
-    super(name,email);
-  }
-  
-  editData(newData){
-    data=newData;
-  }
-}
-
-
-let surender= new User("Surender Suthar","surender@gmail.com");
-surender.viewData();
-
-let vishal= new Admin("Vishal","vishal@gmail.com");
-vishal.editData("Website hacked by vishal");
-
-surender.viewData();
+console.log("a-b:",a-b);
