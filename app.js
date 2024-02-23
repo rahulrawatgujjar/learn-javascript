2d696221e260e16b6ac57b0079d3cb625019e960
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-23 17:22:00 +0530

    how to use super keyword in class

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-// class: it is a template for creation of objects
 class Person{
-  constructor(){
+  constructor(name){
+    this.name=name;
     this.species="homo sapiens";
   }
 
   eat(){
     console.log("eat");
   }
-
-  sleep(){
-    console.log("sleep");
-  }
-
-  work(){
-    console.log("do nothing!");
-  }
 }
 
-// Inheritance:
-//      child class inherits properties and method of parent class
+// super keyword:
+//    it is used to invoke constructor of parent class before using this or returning from child constructor
+//    it is also used to pass argument for parent class constructor
+//    it is also used to access properties and method of parent class in child class using super."name_of_property_or_method()"
+
 class Engineer extends Person{
+  constructor(name,branch){
+    super(name);
+    this.branch=branch;
+  }
+
   work(){
+    super.eat();
     console.log("Solve problems and build something.")
   }
 }
 
-let surender= new Engineer();
-surender.eat();  
-surender.work(); // method over-writing
+let surender= new Engineer("Surender Suthar","Civil engg.");
+console.log(surender.name);
+console.log(surender.branch);
 console.log(surender.species);
+surender.work();
