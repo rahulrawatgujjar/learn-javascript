2b85ac52a83a12af52334ffc931889eb7e594cc9
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-23 16:18:11 +0530

    learn about class in js

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-// object: it is an entity which have properties and methods
+class ToyotaCar{
+  start(){
+    console.log("car started");
+  }
 
-const schoolClass= {
-  className: "12th",
-  roomNum: 3,
-  count: 33,
-  printData: function(){
-    console.log(`Class: ${this.className}\nRoom No.: ${this.roomNum}\nTotal number of students: ${this.count}`);
-  },
-  print: function(){
-    console.log("hello i am class object");
+  stop(){
+    console.log("car stopped");
+  }
+
+  setName(name){
+    this.carName=name;
+  }
+
+  showName(){
+    console.log(this.carName);
   }
 }
 
-
-const student= {
-  fullName: "Rahul Rawat",
-  marks: 98,
-  printMarks: function (){
-    console.log(this.marks);
-  },
-  printName(){
-    console.log(this.fullName);
-  },
-  print: function(){
-    console.log("hello i am student object.");
-  }
-};
-
-// Prototype: every js object has a special property, which is known as prototype, which itself is an object
-// Here we made schoolClass object, a prototype of student object so that we can access properties and methods of schoolClass object in student object
-student.__proto__=schoolClass;
-
-// Example 
-console.log("Name of student:",student.fullName);
-console.log("Marks of student:");
-student.printMarks()
-console.log("Class of student:",student.className);
-console.log("Data about class of student:");
-student.printData()
-// If object and prototype has same method, then objects method will be used
-console.log("Output of function defined in both:");
-student.print()
+let fortuner= new ToyotaCar();
+fortuner.setName("Fortuner");
+fortuner.showName();
+console.log(fortuner);
