68c5527431e4b99b7428dc7a8b3bd02b8c1de812
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-24 15:51:11 +0530

    async and await in js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// Promise: 
-// It is an object. It is returned to us.
-// It has 3 states pending, fullfilled and rejected
-// It takes a funtion as parameter which has 2 handler i.e. resolve and reject
-//  which themselves are callbacks provided by js
+// Async-Await :
+//  async function alway return a promise
+//  await pauses the execution of sorrounding async function until the promise is settled
+
+async function getAllData(){
+  await getData(1);
+  await getData(2);
+  await getData(3);
+}
+
+const output= getAllData();
+console.log(output);
+
 function getData(id){
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
@@ -15,40 +23,3 @@ function getData(id){
     },5000);
   });
 }
-
-// Promise chaining
-
-getData(1).then((res)=>{
-  console.log(res);
-  return getData(2);
-}).then((res)=>{
-  console.log(res);
-  return getData(3);
-}).then((res)=>{
-  console.log(res);
-  return getData(4);
-}).then((res)=>{
-  console.log(res);
-});
-
-// let p1= getData(1);
-// p1.then((res)=>{
-//   console.log(res);
-//   p2=getData(2);
-//   p2.then((res)=>{
-//     console.log(res);
-//     p3=getData(3);
-//     p3.then((res)=>{
-//       console.log(res);
-//     });
-//   });
-// });
-
-// p1.catch((err)=>{
-//   console.log(err);
-// })
-
-
-
-// then for getting res if fullfilled
-// catch for getting err if rejected
