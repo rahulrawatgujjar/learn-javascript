2049ffb48dd91414a16c227ccfa3e24d7d6f8fdf
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-24 15:14:30 +0530

    all about promise in js

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-// Callback hell:
-// Nested callbacks stacked below one another forms a pyramid like structure
-// which is difficult to understand
-
-function getData(id,getNextData){
-  setTimeout(()=>{
-    console.log(`Data ${id}`);
-    if (getNextData){
-      getNextData();
-    }
-  },2000);
+// Promise: 
+// It is an object. It is returned to us.
+// It has 3 states pending, fullfilled and rejected
+// It takes a funtion as parameter which has 2 handler i.e. resolve and reject
+//  which themselves are callbacks provided by js
+function getData(id){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      if (id<=100){
+        console.log(`Data ${id}`);
+        resolve("success");
+      } else {
+        reject("fail");
+      }
+    },5000);
+  });
 }
 
-// In this way all will start their timer at same time and wait parallel and give output at same time
-
-// getData(1);
-// getData(2);
-// getData(3);
-
-getData(1,()=>{
-  getData(2,()=>{
-    getData(3,()=>{
-      getData(4);
+// Promise chaining
+let p1= getData(1);
+p1.then((res)=>{
+  console.log(res);
+  p2=getData(2);
+  p2.then((res)=>{
+    console.log(res);
+    p3=getData(3);
+    p3.then((res)=>{
+      console.log(res);
     });
   });
 });
 
+p1.catch((err)=>{
+  console.log(err);
+})
 
+
+
+// then for getting res if fullfilled
+// catch for getting err if rejected
