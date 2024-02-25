af9ebc498b698a9bebf75b27b7e4e3b87cc80109
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-25 19:31:06 +0530

    created a currency convertor

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-// fetch api:
-//  It provide interface for fetching(sending and receiving) resources
-//  It uses request and response objects
-//  The fetch() method is used to fetch data
-//  It return a promise with result as a JSON
-//  We have to convert it into javascript object using json() method which is also using promise
+const BASE_URL= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies"
 
-const btn= document.querySelector("#btn1");
-const para= document.querySelector("#para1");
+const dropdowns= document.querySelectorAll(".drop-down select");
+const btn= document.querySelector("form button");
+const fromCurr= document.querySelector(".from select");
+const toCurr= document.querySelector(".to select")
+const msg= document.querySelector(".msg")
+const icon= document.querySelector("#ex-icon");
 
-
-const URL = "https://cat-fact.herokuapp.com/facts";
-
-const getFacts= async ()=>{
-  console.log("Getting data..........");
-  let response= await fetch(URL);
-  console.log(response);
-  let data= await response.json(); 
-  console.log(data)
-  para.innerText=data[0].text;
+for(let select of dropdowns){
+  for( let currCode in countryList){
+    let newOpt= document.createElement("option");
+    newOpt.innerText=currCode;
+    newOpt.value=currCode;
+    if (select.name=="from" && currCode=="USD"){
+      newOpt.selected="selected";
+    } else if (select.name=="to" && currCode=="INR") {
+      newOpt.selected="selected";
+    }
+    select.append(newOpt);
+  }
+  select.addEventListener("change",(evt)=>{
+    updateFlag(evt.target);
+  });
 }
 
-btn.addEventListener("click",getFacts);
+const updateFlag = (element)=>{
+  let currCode=element.value;
+  let countryCode=countryList[currCode];
+  let img= element.parentElement.querySelector("img");
+  img.src=`https://flagsapi.com/${countryCode}/flat/64.png`;
+}
+
+
+const updateExchangeRate= async ()=>{
+  let amount= document.querySelector(".amount input");
+  amtVal=Number(amount.value);
+  if ( Number.isNaN(amtVal) || amtVal<1 ){
+    amtVal=1;
+    amount.value="1";
+  }
+  const URL= `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response= await fetch(URL);
+  let data= await response.json();
+  let rate=data[toCurr.value.toLowerCase()];
+  let finalAmt=amtVal*rate;
+  msg.innerText=`${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`
+}
+
+btn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load",()=>{
+  updateExchangeRate();
+})
+
+icon.addEventListener("click",()=>{
+  temp=fromCurr.value;
+  fromCurr.value=toCurr.value;
+  toCurr.value=temp;
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+})
